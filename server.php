2ba8021d7e0f10a22cd129bef7cb10e8703babd5
gab1144 <gabry.rinciari04@gmail.com>
2022-12-05 22:41:33 +0100

    [add] bonus 2

--- server.php
+++ server.php
@@ -4,5 +4,11 @@ $string = file_get_contents('dischi.json');
 
 $list = json_decode($string, true);
 
+if(($_GET['genre']!= "")){
+  $list = array_filter($list, function($value) {
+    return $value['genre'] == $_GET['genre'];
+  });
+}
+
 header('Content-Type: application/json');
 echo json_encode($list);
