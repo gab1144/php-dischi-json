f2b027bb6e55fa346a78ff1de3155ecbf71630ab
gab1144 <gabry.rinciari04@gmail.com>
2022-12-06 01:30:01 +0100

    [change] layout

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
   <!-- Font awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- CSS -->
@@ -30,13 +32,31 @@
     <div class="container">
       <h1>Dischi</h1>
 
-      <div class="input-group mb-3">
-        <input type="text" v-model="newTitle">
-        <input type="text" v-model="newAuthor">
-        <input type="text" v-model="newYear">
-        <input type="text" v-model="newPoster">
-        <input type="text" v-model="newGenre">
-        <button @click="addRecord()" class="btn btn-outline-warning" type="button" id="button-add">Inserisci</button>
+      <div class="input-record">
+        <div>
+          <span>Titolo album</span>
+          <input type="text" v-model="newTitle">
+        </div>
+        <div>
+          <span>Autore</span>
+          <input type="text" v-model="newAuthor">
+        </div>
+        <div>
+          <span>Anno di pubblicazione</span>
+          <input type="text" v-model="newYear">
+        </div>
+        <div>
+          <span>Immagine poster (url)</span>
+          <input type="text" v-model="newPoster">
+        </div>
+        <div>
+          <span>Genere</span>
+          <input type="text" v-model="newGenre">
+        </div>
+
+        <div class="button-add-area">
+          <button @click="addRecord()" class="btn btn-outline-warning" type="button" id="button-add">Inserisci</button>
+        </div>
         </div>
         <div class="record-area">
           <div class="record-card"
